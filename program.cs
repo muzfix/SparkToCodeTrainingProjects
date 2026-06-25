fba2222ea6b8c9c8e4ec8849ec8c75adb214d086
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-06-25 15:14:44 +0400

    Add comments and updates to program.cs for review

---
+++ program.cs
@@ -0,0 +1,14 @@
+namespace FirstCsharpProject
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+
+        }
+    }
+}
+//Add first comment
+// add second comment
+// hello this is a new addition taht must be checked before merging.
