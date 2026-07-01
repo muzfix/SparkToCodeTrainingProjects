ab6d6a8faf079dbf1ce15787c1640b76d3039d9d
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-07-01 17:00:16 +0400

    sessions 1 and 2 practice with karim added

---
+++ Solution1/CsharpFundamentalsPart1/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace CsharpFundamentalsPart1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+
+            // Task 1:
+            // Create a console application that prompts the user for their name, age, height, and whether they are a student.
+            // Then display the collected information back to the user.
+
+            Console.WriteLine("Enter your name: ");
+            string userName = Console.ReadLine();
+
+            Console.WriteLine("Enter your age: ");
+            int userAge = int.Parse(Console.ReadLine());
+
+            Console.WriteLine("Enter your height in meters: ");
+            double userHeight = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("Are you a student? (y/n): ");
+            string userInput = Console.ReadLine();
+            bool isStudent = false;
+            if(userInput == "y")
+            {
+                Console.WriteLine("You are a student.");
+            }
+            else
+            {
+                Console.WriteLine("You are not a student.");
+            }
+
+
+        }
+    }
+}
---
+++ Solution1/LoopsPractice/Program.cs
@@ -0,0 +1,64 @@
+﻿namespace LoopsPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Looping
+            // 1- for loop => countable loop
+            // 2- While loop => uncountable loop (conditional)
+            //
+            //For loop:
+            // for(start; end; increment)
+            // {
+            //     code to be repeated
+            // }
+
+            int counter = 0;
+
+            for(counter = 10; counter >= 5; counter--)
+            {
+                Console.WriteLine("!");
+                Console.WriteLine("Certin message loop counter is at: " + counter);
+            }
+
+            ///////////////////////////////////////////////////////////////////////////
+
+            bool exitDecision = false;
+            int userChoice;
+            string userInput;
+
+
+            //while (exitDecision == false)
+            do 
+            {
+                Console.WriteLine("print menu");
+
+                
+                try
+                {
+                    Console.WriteLine("Do you want to exit? (1/2)");
+                    userChoice = int.Parse(Console.ReadLine());
+                }
+                catch (Exception ex)
+                {
+                    Console.WriteLine(ex.Message);
+                    Console.WriteLine("Invalid input. Please enter a number.");
+                    continue;
+                }
+
+                Console.WriteLine("Do you want to exit? (y/n)");
+                userInput = Console.ReadLine();
+
+                if (userInput == "y")
+                {
+                    exitDecision = true;
+                }
+            } while (exitDecision == false);
+
+
+
+
+        }
+    }
+}
