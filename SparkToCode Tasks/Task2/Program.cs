026bb4a8570ffdecb0d9cebff288a56f7fc4a24f
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-07-04 22:09:15 +0400

    Task 1: Countdown Timer

---
+++ SparkToCode Tasks/Task2/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task2
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            // Csharp fundamentals part 2
+            // Task 1: Countdown Timer
+            Console.WriteLine("Enter a number for the countdown: ");
+            int countTimer = int.Parse(Console.ReadLine());
+            for (int i = countTimer; i > 0; i--)
+            {
+                Console.WriteLine(i);
+                if (i == 1)
+                {
+                    Console.WriteLine("Liftoff!");
+                }
+            }
+        }
+    }
+}
