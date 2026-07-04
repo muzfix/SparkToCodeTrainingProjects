835b481a0f2cfbb178e681671dc3cf61bae466ef
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-07-04 22:16:26 +0400

    Task 2: Sum of numbers 1 to N

--- SparkToCode Tasks/Task2/Program.cs
+++ SparkToCode Tasks/Task2/Program.cs
@@ -6,16 +6,32 @@
         {
             // Csharp fundamentals part 2
             // Task 1: Countdown Timer
-            Console.WriteLine("Enter a number for the countdown: ");
-            int countTimer = int.Parse(Console.ReadLine());
-            for (int i = countTimer; i > 0; i--)
+            //Console.WriteLine("Enter a number for the countdown: ");
+            //int countTimer = int.Parse(Console.ReadLine());
+            //for (int i = countTimer; i > 0; i--)
+            //{
+            //    Console.WriteLine(i);
+            //    if (i == 1)
+            //    {
+            //        Console.WriteLine("Liftoff!");
+            //    }
+            //}
+
+            //Task 2: Sum of numbers 1 to N
+            Console.WriteLine("Enter a positive whole number to calculate the sum from 1 to N: ");
+            int userNum = int.Parse(Console.ReadLine());
+            int userSum = 0;
+
+            for (int i = userNum; i > 0; i--)
             {
-                Console.WriteLine(i);
-                if (i == 1)
-                {
-                    Console.WriteLine("Liftoff!");
-                }
+                userSum += i;
             }
+
+            Console.WriteLine("Sum of numbers from 1 to " + userNum + " is: " + userSum);
+
+
+
+
         }
     }
 }
