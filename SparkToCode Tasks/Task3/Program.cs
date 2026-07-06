bb0d9f7ac719cbebdbab5ffc4fcd5d35e27d8a34
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-07-06 15:03:00 +0400

    Task 2 Power & Root Explorer

--- SparkToCode Tasks/Task3/Program.cs
+++ SparkToCode Tasks/Task3/Program.cs
@@ -5,13 +5,23 @@
         static void Main(string[] args)
         {
             //Task1 Absolute Difference
-            Console.WriteLine("Enter first number: ");
-            int userNum = int.Parse(Console.ReadLine());
-            Console.WriteLine("Enter second number: ");
-            int userNum2 = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter first number: ");
+            //int userNum = int.Parse(Console.ReadLine());
+            //Console.WriteLine("Enter second number: ");
+            //int userNum2 = int.Parse(Console.ReadLine());
 
-            int subResult = userNum - userNum2;
-            Console.WriteLine("The absolute difference is: " + Math.Abs(subResult));
+            //int subResult = userNum - userNum2;
+            //Console.WriteLine("The absolute difference is: " + Math.Abs(subResult));
+
+            //Task 2 Power & Root Explorer
+
+            Console.WriteLine("Enter a number: ");
+            double userNum = double.Parse(Console.ReadLine());
+
+            Console.WriteLine("The number " + userNum + " to the power of 2 is " + Math.Pow(userNum,2)  );
+            Console.WriteLine("The square root of " + userNum + " is " + Math.Sqrt(userNum));
+
+
 
 
         }
