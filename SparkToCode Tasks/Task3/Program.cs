4518881eb9c35dc428db47587115f6eb25f8bd32
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-07-06 14:58:26 +0400

    Created Part 3 questions file "Task3" and completed Task1 Absolute Difference

---
+++ SparkToCode Tasks/Task3/Program.cs
@@ -0,0 +1,19 @@
+﻿namespace Task3
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task1 Absolute Difference
+            Console.WriteLine("Enter first number: ");
+            int userNum = int.Parse(Console.ReadLine());
+            Console.WriteLine("Enter second number: ");
+            int userNum2 = int.Parse(Console.ReadLine());
+
+            int subResult = userNum - userNum2;
+            Console.WriteLine("The absolute difference is: " + Math.Abs(subResult));
+
+
+        }
+    }
+}
