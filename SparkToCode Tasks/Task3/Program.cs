3699964ac06a3943605fd391998e5d155999a10b
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-07-12 12:40:43 +0400

    Task 4 Subscription end date

--- SparkToCode Tasks/Task3/Program.cs
+++ SparkToCode Tasks/Task3/Program.cs
@@ -21,12 +21,21 @@
             //Console.WriteLine("The number " + userNum + " to the power of 2 is " + Math.Pow(userNum,2)  );
             //Console.WriteLine("The square root of " + userNum + " is " + Math.Sqrt(userNum));
 
-            //Task 3 Name Formatter
-            Console.WriteLine("Enter your full name: ");
-            string userName = Console.ReadLine();
-            Console.WriteLine("Your name in uppercase: " + userName.ToUpper());
-            Console.WriteLine("Your name in uppercase: " + userName.ToLower());
-            Console.WriteLine("The number of characters in the name you entered is: " + userName.Length);
+            ////Task 3 Name Formatter
+            //Console.WriteLine("Enter your full name: ");
+            //string userName = Console.ReadLine();
+            //Console.WriteLine("Your name in uppercase: " + userName.ToUpper());
+            //Console.WriteLine("Your name in uppercase: " + userName.ToLower());
+            //Console.WriteLine("The number of characters in the name you entered is: " + userName.Length);
+
+            //Task 4 Subscription end date 
+            Console.WriteLine("Enter the number of days for your trial that starts today: ");
+            int userTrial = int.Parse(Console.ReadLine());
+            DateTime trial = DateTime.Today.AddDays(userTrial);
+
+            Console.WriteLine("Your trial starts today and ends on: " + trial.ToString("yyyy-MM-dd"));
+
+
 
 
 
