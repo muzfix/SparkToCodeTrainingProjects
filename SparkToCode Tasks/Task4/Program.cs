42c68da5d2c5cdbfea35cf1ce7cd9574605dccbd
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-07-15 13:21:10 +0400

    Task 2 Square Number Function

--- SparkToCode Tasks/Task4/Program.cs
+++ SparkToCode Tasks/Task4/Program.cs
@@ -1,20 +1,33 @@
-﻿namespace Task4
+﻿using System.Numerics;
+
+namespace Task4
 {
     internal class Program
     {
         //Task 1 Print Welcome Message
-        static void PrintWelcome(string userName)
+        //static void PrintWelcome(string userName)
+        //{
+        //    Console.WriteLine("Welcome, " + userName + " to the app!");
+        //}
+
+
+        //Task 2 Square Number Function
+        static int Square(int userNumSq)
         {
-            Console.WriteLine("Welcome, " + userName + " to the app!");
+            userNumSq = userNumSq * userNumSq;
+            return userNumSq;
         }
 
         static void Main(string[] args)
         {
-            Console.WriteLine("Please enter your name: ");
-            string userName = Console.ReadLine();
+            //Console.WriteLine("Please enter your name: ");
+            //string userName = Console.ReadLine();
 
-            Program.PrintWelcome(userName);
+            //Program.PrintWelcome(userName);
 
+            Console.WriteLine("Enter a number to be squared: ");
+            int userNumSq = int.Parse(Console.ReadLine());
+            Console.WriteLine("The squared number is: " + Square(userNumSq));
 
         }
     }
