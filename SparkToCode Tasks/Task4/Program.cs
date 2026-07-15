4293b364711e2081688d794c91c9d481ea6744a7
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-07-15 12:45:33 +0400

    Task 1 Print Welcome Message

---
+++ SparkToCode Tasks/Task4/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task4
+{
+    internal class Program
+    {
+        //Task 1 Print Welcome Message
+        static void PrintWelcome(string userName)
+        {
+            Console.WriteLine("Welcome, " + userName + " to the app!");
+        }
+
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter your name: ");
+            string userName = Console.ReadLine();
+
+            Program.PrintWelcome(userName);
+
+
+        }
+    }
+}
