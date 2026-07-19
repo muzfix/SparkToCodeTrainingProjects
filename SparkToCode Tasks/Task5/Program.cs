206d0b365ee2e4862b6eb6333500bd7fba8855ae
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-07-19 17:13:27 +0400

    Task 2: Dynamic To-Do List

--- SparkToCode Tasks/Task5/Program.cs
+++ SparkToCode Tasks/Task5/Program.cs
@@ -5,17 +5,33 @@
         static void Main(string[] args)
         {
             //Task 1: fixed grades array
-            int[] grades = new int[5];
-            for (int i = 0; i < grades.Length; i++)
-            {
-                Console.WriteLine("enter a grade: ");
-                grades[i] = int.Parse(Console.ReadLine());
-            }
+            //int[] grades = new int[5];
+            //for (int i = 0; i < grades.Length; i++)
+            //{
+            //    Console.WriteLine("enter a grade: ");
+            //    grades[i] = int.Parse(Console.ReadLine());
+            //}
 
-            foreach (int grade in grades)
+            //foreach (int grade in grades)
+            //{
+            //    Console.WriteLine(grade);
+            //}
+
+            //Task 2: Dynamic To-Do List
+            List<string> list = new List<string>();
+
+            for (int i = 0; i < 5; i++)
             {
-                Console.WriteLine(grade);
+                Console.WriteLine("Enter a task to add to the to-do list: ");
+                list.Add(Console.ReadLine());
+
             }
+            foreach (string s in list)
+            {
+                Console.WriteLine("- "+ s);
+            }
+            
+
         }
     }
 }
