9bb61f98dd6de0e90435bc0d3393d90dda81ad97
Muhannad Hilal Al-Abri <muzfix4@gmail.com>
2026-07-19 17:04:28 +0400

    Task 1: fixed grades array

---
+++ SparkToCode Tasks/Task5/Program.cs
@@ -0,0 +1,21 @@
+﻿namespace Task5
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            //Task 1: fixed grades array
+            int[] grades = new int[5];
+            for (int i = 0; i < grades.Length; i++)
+            {
+                Console.WriteLine("enter a grade: ");
+                grades[i] = int.Parse(Console.ReadLine());
+            }
+
+            foreach (int grade in grades)
+            {
+                Console.WriteLine(grade);
+            }
+        }
+    }
+}
